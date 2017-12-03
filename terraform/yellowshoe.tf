d129d12313edc76b6821c28467c98c46857431df
Mark Brown <markbrown4@gmail.com>
2017-12-03 16:40:50 +0000

    update security group

--- terraform/yellowshoe.tf
+++ terraform/yellowshoe.tf
@@ -70,9 +70,9 @@ resource "aws_security_group" "main" {
   vpc_id      = "${aws_vpc.main.id}"
 
   ingress {
-    protocol    = "tcp"
-    from_port   = 22
-    to_port     = 22
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
 
