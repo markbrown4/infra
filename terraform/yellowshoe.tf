192ba6b6ec263d1637f1751ad08bdc9dbc44fa34
Mark Brown <markbrown4@gmail.com>
2017-12-03 16:48:00 +0000

    update security group: allow TCP on 22 and 80

--- terraform/yellowshoe.tf
+++ terraform/yellowshoe.tf
@@ -70,9 +70,16 @@ resource "aws_security_group" "main" {
   vpc_id      = "${aws_vpc.main.id}"
 
   ingress {
-    protocol    = "-1"
-    from_port   = 0
-    to_port     = 0
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
     cidr_blocks = ["0.0.0.0/0"]
   }
 
